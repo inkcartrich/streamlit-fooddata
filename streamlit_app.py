f6dd157afe8abf0f58893e9ed0af703b9ad02b3f
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 14:41:22 -0500

    Drop selection table

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,24 +50,3 @@ product_selection = st.selectbox(
 
 st.text(f"Selected brand: {brand_selection}")
 st.text(f"Selected product: {product_selection}")
-
-def dataframe_with_selections(df):
-    df_with_selections = df.copy()
-    df_with_selections.insert(0, "Select", False)
-
-    # Get dataframe row-selections from user with st.data_editor
-    edited_df = st.data_editor(
-        df_with_selections,
-        hide_index=True,
-        column_config={"Select": st.column_config.CheckboxColumn(required=True)},
-        disabled=df.columns,
-    )
-
-    # Filter the dataframe using the temporary column, then drop the column
-    selected_rows = edited_df[edited_df.Select]
-    return selected_rows.drop('Select', axis=1)
-
-
-selection = dataframe_with_selections(df_brand_products)
-st.write("Your selection:")
-st.write(selection)
