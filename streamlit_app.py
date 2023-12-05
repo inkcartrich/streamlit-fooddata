6d6237def6b4367057dfb063800bab07d923e691
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 14:55:20 -0500

    Add df_selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,13 +17,11 @@ st.subheader("Explore the USDA FoodData Central dataset!")
 #
 
 selector_query = f"""
-SELECT DISTINCT BRAND_OWNER, BRAND_NAME, BRANDED_FOOD_CATEGORY from BRANDED_FOOD
+SELECT DISTINCT BRAND_OWNER, BRAND_NAME from BRANDED_FOOD
     WHERE BRANDED_FOOD.BRAND_OWNER IS NOT NULL AND
     LENGTH(BRANDED_FOOD.BRAND_OWNER) > 0 AND
     BRANDED_FOOD.BRAND_NAME IS NOT NULL AND
-    LENGTH(BRANDED_FOOD.BRAND_NAME) > 1 AND
-    BRANDED_FOOD.BRANDED_FOOD_CATEGORY IS NOT NULL AND
-    LENGTH(BRANDED_FOOD.BRANDED_FOOD_CATEGORY) > 1
+    LENGTH(BRANDED_FOOD.BRAND_NAME) > 1 
 """
 
 df_selector = conn.query(selector_query, ttl=600)
@@ -32,11 +30,14 @@ st.text("Searching " + str(len(df_selector)) + " brand-name products.")
 
 df_selector['concat'] = df_selector['BRAND_OWNER'] + ' - ' + df_selector['BRAND_NAME'] 
 
-brand_selection = st.selectbox(
+selection = st.selectbox(
     "Select or search for a product:",
     df_selector['concat'],
     None,
     format_func=lambda x: capwords(x)
 )
 
-st.text(brand_selection)
+df_selection = df_selector[df_selector['concat'] == selection]
+
+st.dataframe(df_selection)
+
