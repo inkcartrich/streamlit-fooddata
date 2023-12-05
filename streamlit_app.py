07c7979aa72bf03cc15f6d7dd46841e6926e29cc
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 15:01:04 -0500

    Update detail query

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,6 @@ detail_query = f"""
 SELECT * FROM BRANDED_FOOD
     WHERE BRANDED_FOOD.BRAND_OWNER = '{df_selection['BRAND_OWNER']}' AND
     BRANDED_FOOD.BRAND_NAME = '{df_selection['BRAND_NAME']}'
-    LIMIT 1
 """
 
 df_detail = conn.query(detail_query, ttl=600)
