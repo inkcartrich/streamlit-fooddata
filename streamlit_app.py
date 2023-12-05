2a98ab2674af047b638b220feee3dc5cdf77c975
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 13:34:27 -0500

    Rollback

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.title("USDA FoodData Central")
 st.subheader("Explore the USDA FoodData Central dataset!")
 
 suggestions_query = f"""
-SELECT DISTINCT BRAND_OWNER, BRAND_NAME from BRANDED_FOOD
+SELECT DISTINCT BRAND_OWNER, BRAND_NAME, BRANDED_FOOD_CATEGORY from BRANDED_FOOD
     WHERE BRANDED_FOOD.BRAND_OWNER IS NOT NULL AND
     LENGTH(BRANDED_FOOD.BRAND_OWNER) AND 
     BRANDED_FOOD.BRAND_NAME IS NOT NULL AND 
@@ -22,8 +22,45 @@ SELECT DISTINCT BRAND_OWNER, BRAND_NAME from BRANDED_FOOD
 
 df_suggest = conn.query(suggestions_query, ttl=600)
 
-df_suggest['concat'] = df_suggest['BRAND_OWNER'] + " - " + df_suggest['BRAND_NAME']
+df_suggest['concat'] = df_suggest['BRAND_OWNER'] + " - " + df_suggest['BRAND_NAME'] + " - " + df_suggest['BRANDED_FOOD_CATEGORY']
 
 st.text("Searching " + str(len(df_suggest)) + " brand-name products.")
 
-selection = st.keyup("Testing")
+value = st_keyup("Enter a value", key="0")
+
+st.write("#")
+
+search_brand_name = st.selectbox(
+    "Select or search for a brand name",
+    df_suggest['concat'],
+    None,
+    format_func=lambda x: capwords(x)
+)
+
+st.write("#")
+
+st.text("Selected product:")
+
+df_selection = df_suggest[df_suggest['concat'] == search_brand_name][['BRAND_OWNER', 'BRAND_NAME']]
+
+selection_owner = df_selection[['BRAND_OWNER']].iloc[0].to_string(index=False)
+selection_product = df_selection[['BRAND_NAME']].iloc[0].to_string(index=False)
+
+st.write("Brand Owner: " + selection_owner)
+st.write("Product: " + selection_product)
+
+query_detailed_string = f"""
+SELECT DISTINCT * from BRANDED_FOOD 
+    WHERE 
+        BRAND_OWNER = '{selection_owner}' AND BRAND_NAME = UPPER('{selection_product}')
+    LIMIT 100;
+"""
+
+# Perform query.
+df_raw = conn.query(query_detailed_string, ttl=600)
+
+df_display = df_raw[['BRAND_OWNER', 'BRAND_NAME', 'GTIN_UPC', 'INGREDIENTS']]
+
+# Print results.
+st.dataframe(df_raw,
+            hide_index=True)
