9eb31aa3f07ab339ef4ca30d53da0e78df0c859a
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 13:46:04 -0500

    Add brand slicer

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,17 +11,33 @@ st.title("USDA FoodData Central")
 
 st.subheader("Explore the USDA FoodData Central dataset!")
 
+#
+# Brands
+#
+
 brand_query = f"""
 SELECT DISTINCT BRAND_OWNER from BRANDED_FOOD
     WHERE BRANDED_FOOD.BRAND_OWNER IS NOT NULL AND
-    LENGTH(BRANDED_FOOD.BRAND_OWNER) > 0
+    LENGTH(BRANDED_FOOD.BRAND_OWNER) > 0 AND
+    COUNT(BRANDED_FOOD.BRAND_NAME) > 1
 """
 
 df_brand = conn.query(brand_query, ttl=600)
 
-st.text("Seaching " + str(len(df_brand)) + " brands.")
+st.text("Searching " + str(len(df_brand)) + " brands.")
+
+brand_slicer = st.selectbox(
+    "Select or search for a brand:",
+    df_brand['concat'],
+    None,
+    format_func=lambda x: capwords(x)
+)
 
 st.write("#")
+
+#
+# Products
+#
 
 suggestions_query = f"""
 SELECT DISTINCT BRAND_OWNER, BRAND_NAME, BRANDED_FOOD_CATEGORY from BRANDED_FOOD
