0509adbb64a0c928a5457f7aac239a653859be71
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 10:31:00 -0500

    Test snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+# streamlit_app.py
 
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection("snowflake")
+
+# Perform query.
+df = conn.query("SELECT * from BRANDED_FOOD LIMIT 100;", ttl=600)
+
+# Print results.
+df
