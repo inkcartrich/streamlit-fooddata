3dcb2bcee41624d31246eef5880c0de764511a68
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 13:28:35 -0500

    Wipe, starting over

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,39 +26,3 @@ df_suggest['concat'] = df_suggest['BRAND_OWNER'] + " - " + df_suggest['BRAND_NAM
 
 st.text("Searching " + str(len(df_suggest)) + " brand-name products.")
 
-st.write("#")
-
-search_brand_name = st.selectbox(
-    "Select or search for a brand name",
-    df_suggest['concat'],
-    None,
-    format_func=lambda x: capwords(x)
-)
-
-st.write("#")
-
-st.text("Selected product:")
-
-df_selection = df_suggest[df_suggest['concat'] == search_brand_name][['BRAND_OWNER', 'BRAND_NAME']]
-
-selection_owner = df_selection[['BRAND_OWNER']].iloc[0].to_string(index=False)
-selection_product = df_selection[['BRAND_NAME']].iloc[0].to_string(index=False)
-
-st.write("Brand Owner: " + selection_owner)
-st.write("Product: " + selection_product)
-
-query_detailed_string = f"""
-SELECT DISTINCT * from BRANDED_FOOD 
-    WHERE 
-        BRAND_OWNER = '{selection_owner}' AND BRAND_NAME = UPPER('{selection_product}')
-    LIMIT 100;
-"""
-
-# Perform query.
-df_raw = conn.query(query_detailed_string, ttl=600)
-
-df_display = df_raw[['BRAND_OWNER', 'BRAND_NAME', 'GTIN_UPC', 'INGREDIENTS']]
-
-# Print results.
-st.dataframe(df_raw,
-            hide_index=True)
