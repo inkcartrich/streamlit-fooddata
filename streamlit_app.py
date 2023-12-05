7f50626c572c64182918414b03ba7073e7d2a12d
Carter <58718246+inkcartrich@users.noreply.github.com>
2023-12-05 14:45:56 -0500

    Go back to the heavy selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,36 +17,26 @@ st.subheader("Explore the USDA FoodData Central dataset!")
 #
 
 selector_query = f"""
-SELECT DISTINCT BRAND_OWNER, BRAND_NAME from BRANDED_FOOD
+SELECT DISTINCT BRAND_OWNER, BRAND_NAME, BRANDED_FOOD_CATEGORY from BRANDED_FOOD
     WHERE BRANDED_FOOD.BRAND_OWNER IS NOT NULL AND
     LENGTH(BRANDED_FOOD.BRAND_OWNER) > 0 AND
     BRANDED_FOOD.BRAND_NAME IS NOT NULL AND
-    LENGTH(BRANDED_FOOD.BRAND_NAME) > 1
+    LENGTH(BRANDED_FOOD.BRAND_NAME) > 1 AND
+    BRANDED_FOOD.BRANDED_FOOD_CATEGORY IS NOT NULL AND
+    LENGTH(BRANDED_FOOD.BRANDED_FOOD_CATEGORY) > 1
 """
 
-df_selector_raw = conn.query(selector_query, ttl=600)
+df_selector = conn.query(selector_query, ttl=600)
 
 st.text("Searching " + str(len(df_selector_raw)) + " brand-name products.")
 
-df_brands = df_selector_raw.drop_duplicates("BRAND_OWNER")
+df_select['concat'] = df_selector['BRAND_OWNER'] + ' - ' + df_selector['BRAND_NAME']
 
 brand_selection = st.selectbox(
-    "Select or search for a brand:",
-    df_brands['BRAND_OWNER'],
-    None,
-    format_func=lambda x: capwords(x)
-)
-
-df_brand_products = df_selector_raw[df_selector_raw['BRAND_OWNER'] == brand_selection]
-
-st.text("Found " + str(len(df_brand_products)) + " products.")
-
-product_selection = st.selectbox(
     "Select or search for a product:",
-    df_brand_products['BRAND_NAME'],
+    df_brands['concat'],
     None,
     format_func=lambda x: capwords(x)
 )
 
-st.text(f"Selected brand: {brand_selection}")
-st.text(f"Selected product: {product_selection}")
+st.text(brand_selection)
